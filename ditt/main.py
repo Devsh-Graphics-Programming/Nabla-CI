8729f3fefdd310fc2ae3d7aef6065fcdf2bdbb50
Hazard (Cyprian Skrzypczak) <jamajpro@gmail.com>
2022-05-21 22:18:50 +0200

    CI for private and public

--- ditt/main.py
+++ ditt/main.py
@@ -4,9 +4,19 @@ import shutil
 import filecmp
 from pathlib import *
 
-NBL_IMAGEMAGICK_EXE = Path('@_NBL_IMAGEMAGICK_EXE_@')
-NBL_PATHTRACER_EXE = Path('@_NBL_PATHTRACER_EXE_@')
-NBL_SCENES_INPUT_TXT = Path('@_NBL_SCENES_INPUT_TXT_@')
+# NBL_IMAGEMAGICK_EXE = Path('@_NBL_IMAGEMAGICK_EXE_@')
+# NBL_PATHTRACER_EXE = Path('@_NBL_PATHTRACER_EXE_@')
+# NBL_SCENES_INPUTS = [ 
+#     (Path('@_NBL_SCENES_INPUT_TXT_@'), "index.html", 'https://artifactory.devsh.eu/Ditt/ci/data/references/' ), 
+#     (Path('@_NBL_PRIVATE_SCENES_INPUT_TXT_@'), "private.html", 'https://artifactory.devsh.eu/Ditt/ci/data/references/')
+# ]
+
+NBL_IMAGEMAGICK_EXE = Path('D:/Programs/ImageMagick-7.1.0-Q16-HDRI/magick.exe')
+NBL_PATHTRACER_EXE = Path('C:/Users/Hazard/Nabla/examples_tests/22.RaytracedAO/bin/raytracedao.exe')
+NBL_SCENES_INPUTS = [ 
+    (Path('C:/Users/Hazard/Nabla/examples_tests/22.RaytracedAO/public_test_scenes.txt'), "index.html", 'https://artifactory.devsh.eu/Ditt/ci/data/references/' ), 
+    (Path('C:/Users/Hazard/Nabla/examples_tests/media/Ditt-Reference-Scenes/private_test_scenes.txt'), "private.html", 'https://artifactory.devsh.eu/Ditt/ci/data/references/')
+]
 NBL_ERROR_THRESHOLD = "0.05" #relative error between reference and generated images, value between 1.0 and 0.0
 NBL_ERROR_TOLERANCE_COUNT = 64          
 CLOSE_TO_ZERO = "0.000001"         
@@ -28,7 +38,7 @@ HTML_R_A_N_D_D_ERROR = 1
 HTML_R_A_N_D_D_PASS = 2
 HTML_R_A_N_D_D_REF = 3
 
-def generateHTMLStatus(_htmlData, _cacheChanged):
+def generateHTMLStatus(_htmlData, _cacheChanged, scenes_input):
     HTML_BODY = '''
     <!DOCTYPE html>
     <html>
@@ -95,8 +105,8 @@ def generateHTMLStatus(_htmlData, _cacheChanged):
             anIndexOfRenderAspect = i + HTML_TUPLE_INPUT_INDEX
 
             aspectRenderData = _htmlRowTuple[anIndexOfRenderAspect]
-            HTML_HYPERLINK_DIFF = 'https://artifactory.devsh.eu/Ditt/ci/data/references/' + _htmlRowTuple[HTML_TUPLE_RENDER_INDEX] + '/' + aspectRenderData[HTML_R_A_N_D_D_DIFF]
-            HTML_HYPERLINK_REF = 'https://artifactory.devsh.eu/Ditt/ci/data/references/' + _htmlRowTuple[HTML_TUPLE_RENDER_INDEX] + '/' + aspectRenderData[HTML_R_A_N_D_D_REF]
+            HTML_HYPERLINK_DIFF = scenes_input[2] + _htmlRowTuple[HTML_TUPLE_RENDER_INDEX] + '/' + aspectRenderData[HTML_R_A_N_D_D_DIFF]
+            HTML_HYPERLINK_REF = scenes_input[2] + _htmlRowTuple[HTML_TUPLE_RENDER_INDEX] + '/' + aspectRenderData[HTML_R_A_N_D_D_REF]
             HTML_ROW_BODY += (  '<td scope="col">' + '<a href="' + HTML_HYPERLINK_DIFF + '">' 
                 + aspectRenderData[HTML_R_A_N_D_D_DIFF] + '</a><br/>'
                 '<a href="'+HTML_HYPERLINK_REF+ '">' 
@@ -118,123 +128,129 @@ def generateHTMLStatus(_htmlData, _cacheChanged):
     </html>
     '''
 
-    htmlFile = open(str(NBL_CI_WORKING_DIR.absolute()) + '/index.html', "w")
+    htmlFile = open(str(NBL_CI_WORKING_DIR.absolute()) + scenes_input[1], "w")
     htmlFile.write(HTML_BODY)
     htmlFile.close()
 
 
 if __name__ == '__main__':
-    if NBL_PATHTRACER_EXE.is_file() and NBL_SCENES_INPUT_TXT.is_file():
-        with open(NBL_SCENES_INPUT_TXT.absolute()) as aFile:
-            inputLines = aFile.readlines()
-
-        os.chdir(NBL_PATHTRACER_EXE.parent.absolute())
-
-        if not NBL_CI_REFERENCES_DIR.is_dir():
-            os.makedirs(str(NBL_CI_REFERENCES_DIR.absolute()))
-
-        htmlData = []
-        cacheChanged = False
-
-        NBL_DUMMY_CACHE_CASE = not bool(Path(str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME).is_file())
-        generatedReferenceCache = str(NBL_PATHTRACER_EXE.parent.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME
-        destinationReferenceCache = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME
-
-        sceneDummyRender = '"../ci/dummy_4096spp_128depth.xml"'
-        executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + sceneDummyRender + ' -TERMINATE'
-        subprocess.run(executor, capture_output=True)
+    if NBL_PATHTRACER_EXE.is_file():
+        for input in NBL_SCENES_INPUTS:
+            input_filepath = input[0]
+            if not input_filepath.is_file():
+                print(f'Scenes input {input_filepath} does not exist!')
+                continue
         
-        # if we start the path tracer first time
-        if NBL_DUMMY_CACHE_CASE:
-            shutil.copyfile(generatedReferenceCache, destinationReferenceCache)
-        elif not filecmp.cmp(destinationReferenceCache, generatedReferenceCache):
-            # fail CI if the reference cache is different that current generated cache
-            cacheChanged = True
-            CI_PASS_STATUS = False
+         
+            with open(input_filepath.absolute()) as aFile:
+                inputLines = aFile.readlines()
 
-        for line in inputLines:
-            if list(line)[0] != ';':
+            os.chdir(NBL_PATHTRACER_EXE.parent.absolute())
 
-                htmlRowTuple = ['', True, ['', '', True, ''], ['', '', True, ''], ['', '', True, ''], ['', '', True, '']]
-                renderPath = line.strip().replace('"', '').split()[0]
-                renderName = os.path.splitext(str(Path(renderPath).name))[0]
-                undenoisedTargetName = 'Render_' + renderName + '_scene'
-                NBL_DUMMY_RENDER_CASE = not bool(Path(str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + undenoisedTargetName + '.exr').is_file())
+            if not NBL_CI_REFERENCES_DIR.is_dir():
+                os.makedirs(str(NBL_CI_REFERENCES_DIR.absolute()))
 
-                # dummy case executes when there is no reference render
-                # or no low discrepancy sequence cache in ci working directory
-                generatedUndenoisedTargetName = str(NBL_PATHTRACER_EXE.parent.absolute()) + '/' + undenoisedTargetName
-                destinationReferenceUndenoisedTargetName = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + undenoisedTargetName
+            htmlData = []
+            cacheChanged = False
 
-                # if we render first time a scene then we need to have a reference of this scene for following ci checks
-                if NBL_DUMMY_RENDER_CASE:
-                    sceneDummyRender = line.strip()
-                    executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + sceneDummyRender + ' -TERMINATE'
+            NBL_DUMMY_CACHE_CASE = not bool(Path(str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME).is_file())
+            generatedReferenceCache = str(NBL_PATHTRACER_EXE.parent.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME
+            destinationReferenceCache = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + NBL_CI_LDS_CACHE_FILENAME
+
+            sceneDummyRender = '"../ci/dummy_4096spp_128depth.xml"'
+            executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + sceneDummyRender + ' -TERMINATE'
+            subprocess.run(executor, capture_output=True)
+            
+            # if we start the path tracer first time
+            if NBL_DUMMY_CACHE_CASE:
+                shutil.copyfile(generatedReferenceCache, destinationReferenceCache)
+            elif not filecmp.cmp(destinationReferenceCache, generatedReferenceCache):
+                # fail CI if the reference cache is different that current generated cache
+                cacheChanged = True
+                CI_PASS_STATUS = False
+
+            for line in inputLines:
+                if list(line)[0] != ';':
+
+                    htmlRowTuple = ['', True, ['', '', True, ''], ['', '', True, ''], ['', '', True, ''], ['', '', True, '']]
+                    renderPath = line.strip().replace('"', '').split()[0]
+                    renderName = os.path.splitext(str(Path(renderPath).name))[0]
+                    undenoisedTargetName = 'Render_' + renderName + '_scene'
+                    NBL_DUMMY_RENDER_CASE = not bool(Path(str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + undenoisedTargetName + '.exr').is_file())
+
+                    # dummy case executes when there is no reference render
+                    # or no low discrepancy sequence cache in ci working directory
+                    generatedUndenoisedTargetName = str(NBL_PATHTRACER_EXE.parent.absolute()) + '/' + undenoisedTargetName
+                    destinationReferenceUndenoisedTargetName = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + undenoisedTargetName
+
+                    # if we render first time a scene then we need to have a reference of this scene for following ci checks
+                    if NBL_DUMMY_RENDER_CASE:
+                        sceneDummyRender = line.strip()
+                        executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + sceneDummyRender + ' -TERMINATE'
+                        subprocess.run(executor, capture_output=True)
+                        if not Path(destinationReferenceUndenoisedTargetName).parent.is_dir():
+                            os.makedirs(str(Path(destinationReferenceUndenoisedTargetName).parent.absolute()))
+                        shutil.copyfile(generatedUndenoisedTargetName + '.exr', destinationReferenceUndenoisedTargetName + '.exr')
+                        shutil.copyfile(generatedUndenoisedTargetName + '_albedo.exr', destinationReferenceUndenoisedTargetName + '_albedo.exr')
+                        shutil.copyfile(generatedUndenoisedTargetName + '_normal.exr', destinationReferenceUndenoisedTargetName + '_normal.exr')
+                        shutil.copyfile(generatedUndenoisedTargetName + '_denoised.exr',destinationReferenceUndenoisedTargetName + '_denoised.exr')
+
+                    scene = line.strip()
+                    htmlRowTuple[HTML_TUPLE_RENDER_INDEX] = renderName
+                    executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + scene + ' -TERMINATE'
                     subprocess.run(executor, capture_output=True)
-                    if not Path(destinationReferenceUndenoisedTargetName).parent.is_dir():
-                        os.makedirs(str(Path(destinationReferenceUndenoisedTargetName).parent.absolute()))
-                    shutil.copyfile(generatedUndenoisedTargetName + '.exr', destinationReferenceUndenoisedTargetName + '.exr')
-                    shutil.copyfile(generatedUndenoisedTargetName + '_albedo.exr', destinationReferenceUndenoisedTargetName + '_albedo.exr')
-                    shutil.copyfile(generatedUndenoisedTargetName + '_normal.exr', destinationReferenceUndenoisedTargetName + '_normal.exr')
-                    shutil.copyfile(generatedUndenoisedTargetName + '_denoised.exr',destinationReferenceUndenoisedTargetName + '_denoised.exr')
 
-                scene = line.strip()
-                htmlRowTuple[HTML_TUPLE_RENDER_INDEX] = renderName
-                executor = str(NBL_PATHTRACER_EXE.absolute()) + ' -SCENE=' + scene + ' -TERMINATE'
-                subprocess.run(executor, capture_output=True)
-
-                if not filecmp.cmp(destinationReferenceCache, generatedReferenceCache):
-                    # fail CI if the reference cache is different that current generated cache
-                    cacheChanged = True
-                    CI_PASS_STATUS = False
-
-
-                anIndex = HTML_TUPLE_INPUT_INDEX
-                outputDiffTerminators = ['', '_albedo', '_normal', '_denoised']
-                for diffTerminator in outputDiffTerminators:
-                    imageDiffFilePath = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + renderName + diffTerminator +"_diff.exr"
-                    imageRefFilepath = destinationReferenceUndenoisedTargetName + diffTerminator + '.exr'
-                    imageGenFilepath = generatedUndenoisedTargetName + diffTerminator + '.exr'
-                    
-                    #create difference image for debugging
-                    diffImageCommandParams = f' "{imageRefFilepath}" "{imageGenFilepath}" -fx "abs(u-v)" "{imageDiffFilePath}"'
-                    executor = str(NBL_IMAGEMAGICK_EXE.absolute()) + diffImageCommandParams
-                    magickDiffImgProcess = subprocess.run(executor, capture_output=False)
-
-                    #calculate the amount of pixels whose relative errors are above NBL_ERROR_THRESHOLD
-                    #logic operators in image magick return 1.0 if true, 0.0 if false 
-                    #image magick convert -compose divide does not work with HDRI, this requiring use of -fx 
-                    diffValueCommandParams = f" {imageRefFilepath} {imageGenFilepath}  -define histogram:unique-colors=true -fx \"(min(u,v)>{CLOSE_TO_ZERO})?((abs(u-v)/min(u,v))>{NBL_ERROR_THRESHOLD}):(max(u,v)<={CLOSE_TO_ZERO})\" -format %c histogram:info:" 
-                    executor = str(NBL_IMAGEMAGICK_EXE.absolute()) + diffValueCommandParams
-                    magickDiffValProcess = subprocess.run(executor, capture_output=True)
-                   
-                    #first histogram line is the amount of black pixels - the correct ones
-                    #second (and last) line is amount of white - pixels whose rel err is above NBL_ERROR_THRESHOLD
-                    histogramOutputLines = magickDiffValProcess.stdout.decode().splitlines()
-                    errorPixelCount = histogramOutputLines[-1].split()[0][:-1] if len(histogramOutputLines) > 1 else "0"
-
-                    diffValueFilepath = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + renderName + diffTerminator + '_diff.txt'
-                    diffValueFile = open(diffValueFilepath, "w")
-                    diffValueFile.write('difference error: ' + str(errorPixelCount))
-                    diffValueFile.close()
-
-                    # threshold for an error, for now we fail CI when the difference is greater then NBL_ERROR_TOLERANCE_COUNT
-                    DIFF_PASS = float(errorPixelCount) <= NBL_ERROR_TOLERANCE_COUNT
-                    if not DIFF_PASS:
+                    if not filecmp.cmp(destinationReferenceCache, generatedReferenceCache):
+                        # fail CI if the reference cache is different that current generated cache
+                        cacheChanged = True
                         CI_PASS_STATUS = False
-                        htmlRowTuple[HTML_TUPLE_PASS_STATUS_INDEX] = False
 
-                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_DIFF] = renderName + diffTerminator + '_diff.exr'
-                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_ERROR] = str(errorPixelCount)
-                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_PASS] = DIFF_PASS
-                    htmlRowTuple[anIndex][HTML_R_A_N_D_D_REF] = 'Render_' + renderName + '_scene'+diffTerminator+".exr"
+                    anIndex = HTML_TUPLE_INPUT_INDEX
+                    outputDiffTerminators = ['', '_albedo', '_normal', '_denoised']
+                    for diffTerminator in outputDiffTerminators:
+                        imageDiffFilePath = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + renderName + diffTerminator +"_diff.exr"
+                        imageRefFilepath = destinationReferenceUndenoisedTargetName + diffTerminator + '.exr'
+                        imageGenFilepath = generatedUndenoisedTargetName + diffTerminator + '.exr'
+                        
+                        #create difference image for debugging
+                        diffImageCommandParams = f' "{imageRefFilepath}" "{imageGenFilepath}" -fx "abs(u-v)" "{imageDiffFilePath}"'
+                        executor = str(NBL_IMAGEMAGICK_EXE.absolute()) + diffImageCommandParams
+                        magickDiffImgProcess = subprocess.run(executor, capture_output=False)
 
-                    anIndex += 1
-                htmlData.append(htmlRowTuple)
+                        #calculate the amount of pixels whose relative errors are above NBL_ERROR_THRESHOLD
+                        #logic operators in image magick return 1.0 if true, 0.0 if false 
+                        #image magick convert -compose divide does not work with HDRI, this requiring use of -fx 
+                        diffValueCommandParams = f" {imageRefFilepath} {imageGenFilepath}  -define histogram:unique-colors=true -fx \"(min(u,v)>{CLOSE_TO_ZERO})?((abs(u-v)/min(u,v))>{NBL_ERROR_THRESHOLD}):(max(u,v)<={CLOSE_TO_ZERO})\" -format %c histogram:info:" 
+                        executor = str(NBL_IMAGEMAGICK_EXE.absolute()) + diffValueCommandParams
+                        magickDiffValProcess = subprocess.run(executor, capture_output=True)
+                    
+                        #first histogram line is the amount of black pixels - the correct ones
+                        #second (and last) line is amount of white - pixels whose rel err is above NBL_ERROR_THRESHOLD
+                        histogramOutputLines = magickDiffValProcess.stdout.decode().splitlines()
+                        errorPixelCount = histogramOutputLines[-1].split()[0][:-1] if len(histogramOutputLines) > 1 else "0"
 
-        generateHTMLStatus(htmlData, cacheChanged)
+                        diffValueFilepath = str(NBL_CI_REFERENCES_DIR.absolute()) + '/' + renderName + '/' + renderName + diffTerminator + '_diff.txt'
+                        diffValueFile = open(diffValueFilepath, "w")
+                        diffValueFile.write('difference error: ' + str(errorPixelCount))
+                        diffValueFile.close()
+
+                        # threshold for an error, for now we fail CI when the difference is greater then NBL_ERROR_TOLERANCE_COUNT
+                        DIFF_PASS = float(errorPixelCount) <= NBL_ERROR_TOLERANCE_COUNT
+                        if not DIFF_PASS:
+                            CI_PASS_STATUS = False
+                            htmlRowTuple[HTML_TUPLE_PASS_STATUS_INDEX] = False
+
+                        htmlRowTuple[anIndex][HTML_R_A_N_D_D_DIFF] = renderName + diffTerminator + '_diff.exr'
+                        htmlRowTuple[anIndex][HTML_R_A_N_D_D_ERROR] = str(errorPixelCount)
+                        htmlRowTuple[anIndex][HTML_R_A_N_D_D_PASS] = DIFF_PASS
+                        htmlRowTuple[anIndex][HTML_R_A_N_D_D_REF] = 'Render_' + renderName + '_scene'+diffTerminator+".exr"
+
+                        anIndex += 1
+                    htmlData.append(htmlRowTuple)
+
+            generateHTMLStatus(htmlData, cacheChanged, input)
     else:
-        print('Path tracer executable or scenes input does not exist!')
+        print('Path tracer executable does not exist!')
         exit(-1)
 
 if not CI_PASS_STATUS:
